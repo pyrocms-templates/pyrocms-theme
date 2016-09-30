6382a5caa90b9e1694abb3b0a7f3ba8e7d598523
Adnan <adnan@websemantics.ca>
2016-09-30 18:49:21 -0400

    Update

---
+++ template/resources/config/design.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'backgrounds' => [
+        'theme::img/backgrounds/urban.jpg',
+        'theme::img/backgrounds/meadow.jpg',
+        'theme::img/backgrounds/forest.jpg',
+        'theme::img/backgrounds/ravine.jpg',
+        'theme::img/backgrounds/mountains.jpg',
+        'theme::img/backgrounds/landscape.jpg',
+        'theme::img/backgrounds/power-lines.jpg'
+    ],
+    'banners'     => [
+        'theme::img/banners/urban.jpg',
+        'theme::img/banners/meadow.jpg',
+        'theme::img/banners/forest.jpg',
+        'theme::img/banners/ravine.jpg',
+        'theme::img/banners/mountains.jpg',
+        'theme::img/banners/landscape.jpg',
+        'theme::img/banners/power-lines.jpg'
+    ]
+];
---
+++ template/src/{{slug|studly_case}}Theme.php
@@ -0,0 +1,22 @@
+<?php namespace {{vendor|studly_case}}\{{slug|studly_case}}Theme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class {{slug|studly_case}}Theme
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ * @package       {{vendor|studly_case}}\{{slug|studly_case}}Module
+ */
+class {{slug|studly_case}}Theme extends Theme
+{
+
+    /**
+     * The admin flag.
+     *
+     * @var bool
+     */
+    protected $admin = true;
+}
